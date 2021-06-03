a41bed94df6a920da254853246e71147b486bd00
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-03 21:56:03 +0200

    Adds SBB module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SbbAutocompleteModule } from '@sbb-esta/angular-business/autocomplete';
+import { SbbDatepickerModule } from '@sbb-esta/angular-business/datepicker';
+import { SbbFormFieldModule } from '@sbb-esta/angular-business/form-field';
+import { SbbTimeInputModule } from '@sbb-esta/angular-business/time-input';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +18,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    HttpClientModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    SbbAutocompleteModule,
+    SbbDatepickerModule,
+    SbbFormFieldModule,
+    SbbTimeInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
