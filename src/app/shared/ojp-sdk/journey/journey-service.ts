3905da2867e234b6ef0305a7f2ff50c4ddb6a9c9
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-09 15:36:29 +0200

    Adds JourneyService and PublicTransportMode journey classes

---
+++ src/app/shared/ojp-sdk/journey/journey-service.ts
@@ -0,0 +1,42 @@
+import { PublicTransportMode } from './public-transport-mode'
+import { XPathOJP } from '../helpers/xpath-ojp'
+
+export class JourneyService {
+    public journeyRef: string;
+    public ptMode: PublicTransportMode;
+    public agencyID: string;
+    public serviceLineNumber: string | null
+    public journeyNumber: string | null
+
+    constructor(journeyRef: string, ptMode: PublicTransportMode, agencyID: string) {
+        this.journeyRef = journeyRef;
+        this.ptMode = ptMode;
+        this.agencyID = agencyID;
+        this.serviceLineNumber = null;
+        this.journeyNumber = null;
+    }
+
+    public static initFromTripLeg(tripLegNode: Node): JourneyService | null {
+        const serviceNode = XPathOJP.queryNode('ojp:Service', tripLegNode)
+        if (serviceNode === null) {
+            return null;
+        }
+
+        const journeyRef = XPathOJP.queryText('ojp:JourneyRef', serviceNode);
+        const ptMode = PublicTransportMode.initFromServiceNode(serviceNode)
+
+        const ojpAgencyId = XPathOJP.queryText('ojp:OperatorRef', serviceNode);
+        const agencyID = ojpAgencyId?.replace('ojp:', '');
+
+        if (!(journeyRef && ptMode && agencyID)) {
+            return null
+        }
+
+        const legService = new JourneyService(journeyRef, ptMode, agencyID);
+
+        legService.serviceLineNumber = XPathOJP.queryText('ojp:PublishedLineName/ojp:Text', serviceNode);
+        legService.journeyNumber = XPathOJP.queryText('ojp:Extension/ojp:PublishedJourneyNumber/ojp:Text', tripLegNode);
+
+        return legService
+    }
+}
---
+++ src/app/shared/ojp-sdk/journey/public-transport-mode.ts
@@ -0,0 +1,30 @@
+import { XPathOJP } from '../helpers/xpath-ojp'
+
+export class PublicTransportMode {
+    public ptMode: string
+    public name: string
+    public shortName: string
+
+    constructor(ptMode: string, name: string, shortName: string) {
+        this.ptMode = ptMode
+        this.name = name
+        this.shortName = shortName
+    }
+
+    public static initFromServiceNode(serviceNode: Node): PublicTransportMode | null {
+        const ptMode = XPathOJP.queryText('ojp:Mode/ojp:PtMode', serviceNode)
+        const name = XPathOJP.queryText('ojp:Mode/ojp:Name/ojp:Text', serviceNode)
+        const shortName = XPathOJP.queryText('ojp:Mode/ojp:ShortName/ojp:Text', serviceNode)
+
+        if (ptMode && name && shortName) {
+            const publicTransportMode = new PublicTransportMode(ptMode, name, shortName)
+            return publicTransportMode
+        }
+
+        return null
+    }
+
+    public isRail(): boolean {
+        return this.ptMode === 'rail';
+    }
+}
