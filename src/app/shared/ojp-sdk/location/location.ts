d46a0b2d80540248a66cca9303187de70fa2ec16
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-09 15:21:43 +0200

    Adds StopPointRef

--- src/app/shared/ojp-sdk/location/location.ts
+++ src/app/shared/ojp-sdk/location/location.ts
@@ -2,14 +2,14 @@ import { XPathOJP } from "../helpers/xpath-ojp";
 import { GeoPosition } from "./geoposition";
 import { StopPlace } from "./stopplace";
 
-type SourceLocationName = 'LocationName' | 'StopPointName'
-
 export class Location {
-  public stopPlace: StopPlace | null
+  public stopPointRef: string | null
   public locationName: string | null
+  public stopPlace: StopPlace | null
   public geoPosition: GeoPosition | null
 
   constructor(contextNode: Node) {
+    this.stopPointRef = XPathOJP.queryText('siri:StopPointRef', contextNode)
     this.locationName = XPathOJP.queryText('ojp:LocationName/ojp:Text', contextNode)
     this.stopPlace = StopPlace.initFromContextNode(contextNode)
     this.geoPosition = GeoPosition.initFromContextNode(contextNode)
