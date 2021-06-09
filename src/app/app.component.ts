fed8735937fd45101ce1884e90e4e6292dfdde10
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-09 16:17:27 +0200

    Basic search form in a SBB accordion / panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SbbAccordion } from '@sbb-esta/angular-business/accordion';
 
 import * as OJP from './shared/ojp-sdk/index'
+
+type SearchState = 'ChooseEndpoints' | 'DisplayTrips'
 
 @Component({
   selector: 'app-root',
@@ -9,9 +12,39 @@ import * as OJP from './shared/ojp-sdk/index'
 })
 export class AppComponent {
   title = 'ojp-demo-app';
+  fromLocation: OJP.Location | null
+  toLocation: OJP.Location | null
 
-  onLocationSelected(location: OJP.Location, originType: string) {
-    console.log('onLocationSelected ' + originType);
-    console.log(location);
+  tripsResponse: OJP.TripsResponse | null
+
+  trips: OJP.Trip[]
+
+  searchState: SearchState = 'ChooseEndpoints'
+
+  @ViewChild(SbbAccordion, { static: true }) firstAccordion: SbbAccordion | undefined;
+
+  constructor() {
+    this.fromLocation = null;
+    this.toLocation = null;
+    this.tripsResponse = null;
+    this.trips = []
+    this.firstAccordion = undefined;
+  }
+
+  onLocationSelected(location: OJP.Location, originType: OJP.JourneyPointType) {
+    if (originType === 'From') {
+      this.fromLocation = location
+    } else {
+      this.toLocation = location
+    }
+  }
+
+  onTripsResponseCompleted(response: OJP.TripsResponse) {
+    this.trips = response.trips
+    this.searchState = 'DisplayTrips'
+  }
+
+  isChoosingEndpoints(): boolean {
+    return this.searchState === 'ChooseEndpoints'
   }
 }
