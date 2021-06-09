3fc97387213bf131c0040be7681234b61182020c
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-09 15:18:21 +0200

    Adds helper for computing the duration (leg, journey)

--- src/app/shared/ojp-sdk/helpers/date-helpers.ts
+++ src/app/shared/ojp-sdk/helpers/date-helpers.ts
@@ -1,3 +1,5 @@
+import { Duration } from '../types/duration'
+
 export class DateHelpers {
   // 2021-06-03 21:38:04
   public static formatDate(d: Date) {
@@ -22,5 +24,29 @@ export class DateHelpers {
   public static formatTimeHHMM(d: Date, separator: string = ':'): string {
     const dateFormatted = DateHelpers.formatDate(d)
     return dateFormatted.substring(11,16);
+  }
+
+  public static computeDuration(durationS: string): Duration {
+    // PT4H19M
+    durationS = durationS.replace('PT', '');
+
+    let hours = 0
+    const hoursMatches = durationS.match(/([0-9]+?)H/);
+    if (hoursMatches) {
+        hours = parseInt(hoursMatches[1])
+    }
+
+    let minutes = 0
+    const minutesMatches = durationS.match(/([0-9]+?)M/);
+    if (minutesMatches) {
+        minutes = parseInt(minutesMatches[1])
+    }
+
+    const duration = <Duration>{
+        hours: hours,
+        minutes: minutes
+    }
+
+    return duration
   }
 }
