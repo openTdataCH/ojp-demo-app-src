baa0cd2c67e4475beb81aa512c9160816d5413df
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-14 09:12:20 +0200

    2 spaces indent

--- src/app/shared/ojp-sdk/location/geoposition.ts
+++ src/app/shared/ojp-sdk/location/geoposition.ts
@@ -6,24 +6,24 @@ export class GeoPosition {
   public latitude: number
 
   constructor(longitude: number, latitude: number) {
-      this.longitude = longitude
-      this.latitude = latitude
+    this.longitude = longitude
+    this.latitude = latitude
   }
 
   public static initFromContextNode(contextNode: Node): GeoPosition | null {
-      const longitudeS = XPathOJP.queryText('ojp:GeoPosition/siri:Longitude', contextNode)
-      const latitudeS = XPathOJP.queryText('ojp:GeoPosition/siri:Latitude', contextNode)
+    const longitudeS = XPathOJP.queryText('ojp:GeoPosition/siri:Longitude', contextNode)
+    const latitudeS = XPathOJP.queryText('ojp:GeoPosition/siri:Latitude', contextNode)
 
-      if (longitudeS === null || latitudeS === null) {
-          return null
-      }
+    if (longitudeS === null || latitudeS === null) {
+      return null
+    }
 
-      const longitude = parseFloat(longitudeS)
-      const latitude = parseFloat(latitudeS)
+    const longitude = parseFloat(longitudeS)
+    const latitude = parseFloat(latitudeS)
 
-      const geoPosition = new GeoPosition(longitude, latitude)
+    const geoPosition = new GeoPosition(longitude, latitude)
 
-      return geoPosition
+    return geoPosition
   }
 
   public asLngLat(): mapboxgl.LngLat {
