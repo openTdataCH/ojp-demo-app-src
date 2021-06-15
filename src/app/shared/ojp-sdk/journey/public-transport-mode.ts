9b0fe13e0ac3870b5425e9de5adad8078a1c124a
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-15 22:58:08 +0200

    Relax even more the PublicTransportMode, keep only the name as required

--- src/app/shared/ojp-sdk/journey/public-transport-mode.ts
+++ src/app/shared/ojp-sdk/journey/public-transport-mode.ts
@@ -2,10 +2,10 @@ import { XPathOJP } from '../helpers/xpath-ojp'
 
 export class PublicTransportMode {
   public ptMode: string
-  public name: string
+  public name: string | null
   public shortName: string | null
 
-  constructor(ptMode: string, name: string, shortName: string | null) {
+  constructor(ptMode: string, name: string | null, shortName: string | null) {
     this.ptMode = ptMode
     this.name = name
     this.shortName = shortName
@@ -13,15 +13,16 @@ export class PublicTransportMode {
 
   public static initFromServiceNode(serviceNode: Node): PublicTransportMode | null {
     const ptMode = XPathOJP.queryText('ojp:Mode/ojp:PtMode', serviceNode)
-    const name = XPathOJP.queryText('ojp:Mode/ojp:Name/ojp:Text', serviceNode)
 
-    if (ptMode && name) {
-      const shortName = XPathOJP.queryText('ojp:Mode/ojp:ShortName/ojp:Text', serviceNode)
-      const publicTransportMode = new PublicTransportMode(ptMode, name, shortName)
-      return publicTransportMode
+    if (ptMode === null) {
+      return null
     }
 
-    return null
+    const name = XPathOJP.queryText('ojp:Mode/ojp:Name/ojp:Text', serviceNode)
+    const shortName = XPathOJP.queryText('ojp:Mode/ojp:ShortName/ojp:Text', serviceNode)
+    const publicTransportMode = new PublicTransportMode(ptMode, name, shortName)
+
+    return publicTransportMode
   }
 
   public isRail(): boolean {
