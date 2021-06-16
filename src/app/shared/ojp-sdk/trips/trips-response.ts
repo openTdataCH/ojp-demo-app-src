f6bf11619bbd13d9d78ad7973e888c35c8a8e36c
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-16 16:42:12 +0200

    2-spaces soft tabs

--- src/app/shared/ojp-sdk/trips/trips-response.ts
+++ src/app/shared/ojp-sdk/trips/trips-response.ts
@@ -3,63 +3,63 @@ import { Trip } from '../trip/trip'
 import { XPathOJP } from '../helpers/xpath-ojp'
 
 export class TripsResponse {
-    public contextLocations: Location[]
-    public trips: Trip[]
+  public contextLocations: Location[]
+  public trips: Trip[]
 
-    constructor(responseXMLText: string) {
-        const responseXML = new DOMParser().parseFromString(responseXMLText, 'application/xml');
+  constructor(responseXMLText: string) {
+    const responseXML = new DOMParser().parseFromString(responseXMLText, 'application/xml');
 
-        this.contextLocations = this.parseContextLocations(responseXML);
-        this.trips = this.parseTrips(responseXML);
-    }
+    this.contextLocations = this.parseContextLocations(responseXML);
+    this.trips = this.parseTrips(responseXML);
+  }
 
-    private parseContextLocations(responseXML: Document): Location[] {
-        let locations: Location[] = [];
+  private parseContextLocations(responseXML: Document): Location[] {
+    let locations: Location[] = [];
 
-        const locationNodes = XPathOJP.queryNodes('//ojp:TripResponseContext/ojp:Places/ojp:Location', responseXML);
-        locationNodes.forEach(locationNode => {
-            const location = Location.initWithOJPContextNode(locationNode)
-            locations.push(location);
-        });
+    const locationNodes = XPathOJP.queryNodes('//ojp:TripResponseContext/ojp:Places/ojp:Location', responseXML);
+    locationNodes.forEach(locationNode => {
+      const location = Location.initWithOJPContextNode(locationNode)
+      locations.push(location);
+    });
 
-        return locations;
-    }
+    return locations;
+  }
 
-    private parseTrips(responseXML: Document): Trip[] {
-        let trips: Trip[] = [];
+  private parseTrips(responseXML: Document): Trip[] {
+    let trips: Trip[] = [];
 
-        const mapContextLocations: Record<string, Location> = {}
-        this.contextLocations.forEach(location => {
-          const stopPlaceRef = location.stopPlace?.stopPlaceRef
-          if (stopPlaceRef) {
-            mapContextLocations[stopPlaceRef] = location
-          }
-        });
+    const mapContextLocations: Record<string, Location> = {}
+    this.contextLocations.forEach(location => {
+      const stopPlaceRef = location.stopPlace?.stopPlaceRef
+      if (stopPlaceRef) {
+        mapContextLocations[stopPlaceRef] = location
+      }
+    });
 
-        const tripResultNodes = XPathOJP.queryNodes('//ojp:TripResult', responseXML);
-        tripResultNodes.forEach(tripResult => {
-            const trip = Trip.initFromTripResultNode(tripResult as Node, this.contextLocations);
-            if (trip) {
-              // Massage locations
-              trip.legs.forEach(leg => {
-                [leg.fromLocation, leg.toLocation].forEach(location => {
-                  if (location.geoPosition === null) {
-                    const stopPointRef = location.stopPointRef
-                    if (stopPointRef && (stopPointRef in mapContextLocations)) {
-                      const contextLocation = mapContextLocations[stopPointRef]
+    const tripResultNodes = XPathOJP.queryNodes('//ojp:TripResult', responseXML);
+    tripResultNodes.forEach(tripResult => {
+      const trip = Trip.initFromTripResultNode(tripResult as Node, this.contextLocations);
+      if (trip) {
+        // Massage locations
+        trip.legs.forEach(leg => {
+          [leg.fromLocation, leg.toLocation].forEach(location => {
+            if (location.geoPosition === null) {
+              const stopPointRef = location.stopPointRef
+              if (stopPointRef && (stopPointRef in mapContextLocations)) {
+                const contextLocation = mapContextLocations[stopPointRef]
 
-                      location.locationName = contextLocation.locationName
-                      location.stopPlace = contextLocation.stopPlace
-                      location.geoPosition = contextLocation.geoPosition
-                    }
-                  }
-                })
-              });
-
-              trips.push(trip);
+                location.locationName = contextLocation.locationName
+                location.stopPlace = contextLocation.stopPlace
+                location.geoPosition = contextLocation.geoPosition
+              }
             }
+          })
         });
 
-        return trips
-    }
+        trips.push(trip);
+      }
+    });
+
+    return trips
+  }
 }
