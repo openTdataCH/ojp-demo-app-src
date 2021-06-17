f88fb587608f5b469baa66e5932c811a31035e34
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-17 12:55:56 +0200

    2-spaces soft tabs

--- src/app/shared/ojp-sdk/trip/path-guidance.ts
+++ src/app/shared/ojp-sdk/trip/path-guidance.ts
@@ -2,84 +2,81 @@ import { XPathOJP } from "../helpers/xpath-ojp";
 import { LinkProjection } from "./link-projection";
 
 export class PathGuidance {
-    public sections: PathGuidanceSection[];
+  public sections: PathGuidanceSection[];
 
-    constructor(sections: PathGuidanceSection[]) {
-        this.sections = sections;
-    }
+  constructor(sections: PathGuidanceSection[]) {
+    this.sections = sections;
+  }
 
-    public static initFromTripLeg(tripLegNode: Node): PathGuidance {
-        const sectionNodes = XPathOJP.queryNodes('ojp:PathGuidance/ojp:PathGuidanceSection', tripLegNode);
+  public static initFromTripLeg(tripLegNode: Node): PathGuidance {
+    const sectionNodes = XPathOJP.queryNodes('ojp:PathGuidance/ojp:PathGuidanceSection', tripLegNode);
 
-        let sections: PathGuidanceSection[] = [];
+    let sections: PathGuidanceSection[] = [];
 
-        sectionNodes.forEach((sectionNode, sectionIdx) => {
-            const pathGuidanceSection = PathGuidanceSection.initFromSectionNode(sectionNode);
-            if (pathGuidanceSection) {
-                sections.push(pathGuidanceSection)
-            }
-        });
+    sectionNodes.forEach((sectionNode, sectionIdx) => {
+      const pathGuidanceSection = PathGuidanceSection.initFromSectionNode(sectionNode);
+      if (pathGuidanceSection) {
+        sections.push(pathGuidanceSection)
+      }
+    });
 
-        const pathGuidance = new PathGuidance(sections);
+    const pathGuidance = new PathGuidance(sections);
 
-        return pathGuidance;
-    }
+    return pathGuidance;
+  }
 }
 
 class PathGuidanceSection {
-    public trackSection: TrackSection | null
-    public guidanceAdvice: string | null
-    public turnAction: string | null
+  public trackSection: TrackSection | null
+  public guidanceAdvice: string | null
+  public turnAction: string | null
 
-    constructor() {
-        this.trackSection = null
-        this.guidanceAdvice = null
-        this.turnAction = null
+  constructor() {
+    this.trackSection = null
+    this.guidanceAdvice = null
+    this.turnAction = null
+  }
+
+  public static initFromSectionNode(sectionNode: Node): PathGuidanceSection {
+    const pathGuidanceSection = new PathGuidanceSection();
+    const trackSectionNode = XPathOJP.queryNode('ojp:TrackSection', sectionNode);
+
+    if (trackSectionNode) {
+      pathGuidanceSection.trackSection = TrackSection.initFromTrackSectionNode(trackSectionNode);
     }
 
-    public static initFromSectionNode(sectionNode: Node): PathGuidanceSection {
-        const pathGuidanceSection = new PathGuidanceSection();
+    pathGuidanceSection.guidanceAdvice = XPathOJP.queryText('ojp:GuidanceAdvice', sectionNode);
+    pathGuidanceSection.turnAction = XPathOJP.queryText('ojp:TurnAction', sectionNode);
 
-        const trackSectionNode = XPathOJP.queryNode('ojp:TrackSection', sectionNode);
-        if (trackSectionNode) {
-            pathGuidanceSection.trackSection = TrackSection.initFromTrackSectionNode(trackSectionNode);
-        }
-
-        pathGuidanceSection.guidanceAdvice = XPathOJP.queryText('ojp:GuidanceAdvice', sectionNode);
-        pathGuidanceSection.turnAction = XPathOJP.queryText('ojp:TurnAction', sectionNode);
-
-        return pathGuidanceSection;
-    }
+    return pathGuidanceSection;
+  }
 }
 
 class TrackSection {
-    public linkProjection: LinkProjection | null;
-    public roadName: string | null;
-    public duration: string | null;
-    public length: number | null;
+  public linkProjection: LinkProjection | null;
+  public roadName: string | null;
+  public duration: string | null;
+  public length: number | null;
 
-    constructor() {
-        this.linkProjection = null;
-        this.roadName = null;
-        this.duration = null;
-        this.length = null;
+  constructor() {
+    this.linkProjection = null;
+    this.roadName = null;
+    this.duration = null;
+    this.length = null;
+  }
+
+  public static initFromTrackSectionNode(trackSectionNode: Node): TrackSection {
+    const trackSection = new TrackSection();
+
+    trackSection.linkProjection = LinkProjection.initFromTrackSectionNode(trackSectionNode);
+    trackSection.roadName = XPathOJP.queryText('ojp:RoadName', trackSectionNode);
+    trackSection.duration = XPathOJP.queryText('ojp:Duration', trackSectionNode);
+
+    const lengthS = XPathOJP.queryText('ojp:Length', trackSectionNode);
+    if (lengthS) {
+      trackSection.length = parseInt(lengthS, 10);
     }
 
-    public static initFromTrackSectionNode(trackSectionNode: Node): TrackSection {
-        const trackSection = new TrackSection();
-
-        trackSection.linkProjection = LinkProjection.initFromTrackSectionNode(trackSectionNode);
-        trackSection.roadName = XPathOJP.queryText('ojp:RoadName', trackSectionNode);
-        trackSection.duration = XPathOJP.queryText('ojp:Duration', trackSectionNode);
-
-        const lengthS = XPathOJP.queryText('ojp:Length', trackSectionNode);
-        if (lengthS) {
-            trackSection.length = parseInt(lengthS, 10);
-        }
-
-        return trackSection
-    }
+    return trackSection
+  }
 }
-
-
-
