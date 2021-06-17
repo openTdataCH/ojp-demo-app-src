a94d7d8567a42286d49f6459f106f21ec67f95cd
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-17 18:31:55 +0200

    No more need of these

--- src/app/shared/services/map.service.ts
+++ src/app/shared/services/map.service.ts
@@ -2,13 +2,6 @@ import { Injectable, EventEmitter } from '@angular/core'
 import mapboxgl from 'mapbox-gl';
 import * as OJP from '../ojp-sdk/index'
 
-type LocationUpdateSource = 'InputEndpoint'
-interface LocationData {
-  location: OJP.Location | null,
-  zoomLevel: number
-  updateSource: LocationUpdateSource,
-}
-
 @Injectable( {providedIn: 'root'} )
 export class MapService {
   public centerAndZoomToEndpointRequested = new EventEmitter<OJP.JourneyPointType>();
