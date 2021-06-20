7693d3bf060585e0617cfa997ebeb64302d2cec2
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-20 20:49:18 +0200

    2-spaces soft tabs

--- src/app/shared/ojp-sdk/journey/journey-service.ts
+++ src/app/shared/ojp-sdk/journey/journey-service.ts
@@ -2,41 +2,41 @@ import { PublicTransportMode } from './public-transport-mode'
 import { XPathOJP } from '../helpers/xpath-ojp'
 
 export class JourneyService {
-    public journeyRef: string;
-    public ptMode: PublicTransportMode;
-    public agencyID: string;
-    public serviceLineNumber: string | null
-    public journeyNumber: string | null
+  public journeyRef: string;
+  public ptMode: PublicTransportMode;
+  public agencyID: string;
+  public serviceLineNumber: string | null
+  public journeyNumber: string | null
 
-    constructor(journeyRef: string, ptMode: PublicTransportMode, agencyID: string) {
-        this.journeyRef = journeyRef;
-        this.ptMode = ptMode;
-        this.agencyID = agencyID;
-        this.serviceLineNumber = null;
-        this.journeyNumber = null;
+  constructor(journeyRef: string, ptMode: PublicTransportMode, agencyID: string) {
+    this.journeyRef = journeyRef;
+    this.ptMode = ptMode;
+    this.agencyID = agencyID;
+    this.serviceLineNumber = null;
+    this.journeyNumber = null;
+  }
+
+  public static initFromTripLeg(tripLegNode: Node): JourneyService | null {
+    const serviceNode = XPathOJP.queryNode('ojp:Service', tripLegNode)
+    if (serviceNode === null) {
+      return null;
     }
 
-    public static initFromTripLeg(tripLegNode: Node): JourneyService | null {
-        const serviceNode = XPathOJP.queryNode('ojp:Service', tripLegNode)
-        if (serviceNode === null) {
-            return null;
-        }
+    const journeyRef = XPathOJP.queryText('ojp:JourneyRef', serviceNode);
+    const ptMode = PublicTransportMode.initFromServiceNode(serviceNode)
 
-        const journeyRef = XPathOJP.queryText('ojp:JourneyRef', serviceNode);
-        const ptMode = PublicTransportMode.initFromServiceNode(serviceNode)
+    const ojpAgencyId = XPathOJP.queryText('ojp:OperatorRef', serviceNode);
+    const agencyID = ojpAgencyId?.replace('ojp:', '');
 
-        const ojpAgencyId = XPathOJP.queryText('ojp:OperatorRef', serviceNode);
-        const agencyID = ojpAgencyId?.replace('ojp:', '');
-
-        if (!(journeyRef && ptMode && agencyID)) {
-            return null
-        }
-
-        const legService = new JourneyService(journeyRef, ptMode, agencyID);
-
-        legService.serviceLineNumber = XPathOJP.queryText('ojp:PublishedLineName/ojp:Text', serviceNode);
-        legService.journeyNumber = XPathOJP.queryText('ojp:Extension/ojp:PublishedJourneyNumber/ojp:Text', tripLegNode);
-
-        return legService
+    if (!(journeyRef && ptMode && agencyID)) {
+      return null
     }
+
+    const legService = new JourneyService(journeyRef, ptMode, agencyID);
+
+    legService.serviceLineNumber = XPathOJP.queryText('ojp:PublishedLineName/ojp:Text', serviceNode);
+    legService.journeyNumber = XPathOJP.queryText('ojp:Extension/ojp:PublishedJourneyNumber/ojp:Text', tripLegNode);
+
+    return legService
+  }
 }
