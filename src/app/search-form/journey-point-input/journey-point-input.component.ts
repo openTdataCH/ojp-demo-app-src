17c27066725b43ec60b2c4f60dc8ebfa41e2ce81
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-23 10:54:48 +0200

    Use the new initializer

--- src/app/search-form/journey-point-input/journey-point-input.component.ts
+++ src/app/search-form/journey-point-input/journey-point-input.component.ts
@@ -41,7 +41,7 @@ export class JourneyPointInputComponent implements OnInit, OnChanges {
         return;
       }
 
-      const coordsLocation = this.matchCoordsInput(searchTerm);
+      const coordsLocation = OJP.Location.initFromLiteralCoords(searchTerm);
       if (coordsLocation) {
         this.lookupLocations = []
         this.handleCoordsPick(coordsLocation)
@@ -94,26 +94,6 @@ export class JourneyPointInputComponent implements OnInit, OnChanges {
 
   public handleTapOnMapButton() {
     this.mapService.centerAndZoomToEndpointRequested.emit(this.endpointType);
-  }
-
-  private matchCoordsInput(inputS: string): OJP.Location | null {
-    inputS = inputS.trim().replace(/\s/g, '');
-
-    const inputMatches = inputS.match(/^([0-9\.]+?),([0-9\.]+?)$/);
-    if (inputMatches === null) {
-      return null
-    }
-
-    let longitude = parseFloat(inputMatches[1])
-    let latitude = parseFloat(inputMatches[2])
-    // In CH always long < lat
-    if (longitude > latitude) {
-      longitude = parseFloat(inputMatches[2])
-      latitude = parseFloat(inputMatches[1])
-    }
-
-    const location = OJP.Location.initWithLngLat(longitude, latitude)
-    return location
   }
 
   private handleCoordsPick(location: OJP.Location) {
