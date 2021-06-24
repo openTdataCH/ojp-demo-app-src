f03e71bffc4e3c0c136d0d3002bd27a284057633
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-24 22:00:59 +0200

    2-spaces soft tabs

--- src/app/shared/ojp-sdk/helpers/xpath-ojp.ts
+++ src/app/shared/ojp-sdk/helpers/xpath-ojp.ts
@@ -1,35 +1,35 @@
 import xpath from 'xpath'
 
 const mapNS = {
-    'siri' : 'http://www.siri.org.uk/siri',
-    'ojp': 'http://www.vdv.de/ojp',
+  'siri' : 'http://www.siri.org.uk/siri',
+  'ojp': 'http://www.vdv.de/ojp',
 };
 
 export class XPathOJP {
-    public static queryNodes(xpathExpression: string, contextNode: Node): Node[] {
-        const queryNS = xpath.useNamespaces(mapNS);
-        const nodes = queryNS(xpathExpression, contextNode) as Node[];
-        return nodes
+  public static queryNodes(xpathExpression: string, contextNode: Node): Node[] {
+    const queryNS = xpath.useNamespaces(mapNS);
+    const nodes = queryNS(xpathExpression, contextNode) as Node[];
+    return nodes
+  }
+
+  public static queryNode(xpathExpression: string, contextNode: Node): Node | null {
+    const nodes = XPathOJP.queryNodes(xpathExpression, contextNode)
+    if (nodes.length === 0) {
+      return null;
     }
 
-    public static queryNode(xpathExpression: string, contextNode: Node): Node | null {
-        const nodes = XPathOJP.queryNodes(xpathExpression, contextNode)
-        if (nodes.length === 0) {
-            return null;
-        }
+    return nodes[0]
+  }
 
-        return nodes[0]
+  public static queryText(xpathExpression: string, contextNode: Node): string | null {
+    const queryNS = xpath.useNamespaces(mapNS);
+    const node = queryNS(xpathExpression, contextNode, true) as Node;
+    if (!node) {
+      return null;
     }
 
-    public static queryText(xpathExpression: string, contextNode: Node): string | null {
-        const queryNS = xpath.useNamespaces(mapNS);
-        const node = queryNS(xpathExpression, contextNode, true) as Node;
-        if (!node) {
-            return null;
-        }
+    const nodeText = node.textContent
 
-        const nodeText = node.textContent
-
-        return nodeText
-    }
+    return nodeText
+  }
 }
