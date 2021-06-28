de2db7dd1ecef8040e73e7555fb4ffc7d712a483
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-29 00:38:54 +0200

    Expose more members

--- src/app/shared/ojp-sdk/request/index.ts
+++ src/app/shared/ojp-sdk/request/index.ts
@@ -1,3 +1,5 @@
+export * from './journey-request/journey-request'
+export * from './journey-request/journey-request-params'
 export * from './location-information/location-information-request'
 export * from './trips-request/trips-request-params'
 export * from './trips-request/trips-request'
