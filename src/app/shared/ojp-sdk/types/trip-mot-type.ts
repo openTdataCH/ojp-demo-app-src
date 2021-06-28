24f973d6d809ee72b4d60ae39d714b456cbca07a
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-06-29 00:33:41 +0200

    Hello TripMotType union type

---
+++ src/app/shared/ojp-sdk/types/trip-mot-type.ts
@@ -0,0 +1,2 @@
+export type TripMotType = 'Default' | 'Walking' | 'Self-Driving Car' | 'Shared Mobility'
+export const TripMotTypes: TripMotType[] = ['Default', 'Walking', 'Self-Driving Car', 'Shared Mobility']
