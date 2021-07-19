940565585163a09d16b225f9ef67df88e9d69abb
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-07-19 22:00:01 +0200

    Patch the stats for multiple-sections journeys

--- src/app/shared/ojp-sdk/journey/journey-response.ts
+++ src/app/shared/ojp-sdk/journey/journey-response.ts
@@ -1,3 +1,4 @@
+import { Duration } from "../shared/duration";
 import { Trip, TripLeg } from "../trip";
 import { JourneySection } from "./journey-section";
 
@@ -11,12 +12,29 @@ export class JourneyResponse {
   public computeAggregatedTrips(): Trip[] {
     let aggregatedTrips: Trip[] = []
     let prevLegs: TripLeg[] = []
+    let prevDistance: number = 0
+    let prevDurationMinutes: number = 0
+    
+    const hasMultipleSections = this.sections.length > 1
 
     this.sections.forEach((section, idx) => {
       const isLast = idx === (this.sections.length - 1)
 
       if (isLast) {
         section.response.trips.forEach(trip => {
+          if (hasMultipleSections) {
+            const firstTrip = this.sections[0].response.trips[0]
+            trip.stats.startDatetime = firstTrip.stats.startDatetime
+
+            // Sum the first sections count which are monomodals
+            trip.stats.transferNo += (this.sections.length - 1)
+
+            trip.stats.distanceMeters += prevDistance
+            
+            const tripDurationMinutes = prevDurationMinutes + trip.stats.duration.totalMinutes
+            trip.stats.duration = Duration.initFromTotalMinutes(tripDurationMinutes)
+          }
+
           trip.legs = prevLegs.concat(trip.legs)
           // TODO - change also TripStats
           // trip.stats = alter with prev legs
@@ -26,8 +44,12 @@ export class JourneyResponse {
       } else {
         const hasTrips = section.response.trips.length > 0
         if (hasTrips) {
+          // Keep the first trip, it contains the MOT-related data.
           const firstTrip = section.response.trips[0]
           prevLegs = prevLegs.concat(firstTrip.legs)
+          prevDistance += firstTrip.stats.distanceMeters
+
+          prevDurationMinutes += firstTrip.stats.duration.totalMinutes
         }
       }
     })
