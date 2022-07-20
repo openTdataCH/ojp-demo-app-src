c9fb4925eef3bab7f99f58fa6240e766961daeab
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-07-20 13:16:13 +0200

    Add support to compute distance to another GeoPosition

--- src/app/shared/ojp-sdk/location/geoposition.ts
+++ src/app/shared/ojp-sdk/location/geoposition.ts
@@ -67,4 +67,20 @@ export class GeoPosition {
   public asPosition(): GeoJSON.Position {
     return [this.longitude, this.latitude]
   }
+
+  // From https://stackoverflow.com/a/27943
+  public distanceFrom(pointB: GeoPosition): number {
+    const R = 6371; // Radius of the earth in km
+    const dLat = (pointB.latitude - this.latitude) * Math.PI / 180;
+    const dLon = (pointB.longitude - this.longitude) * Math.PI / 180;
+    const a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.latitude * Math.PI / 180) * Math.cos(pointB.latitude * Math.PI / 180) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2); 
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    const d = R * c; 
+    const dMeters = Math.round(d * 1000);
+
+    return dMeters;
+  }
 }
