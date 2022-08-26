3e9e47e91d6ef02b0f8f9a7eb1cc6f94a5ba3b0c
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-08-26 14:20:05 +0200

    Use flex for the components, adds the mobility settings (distance, duration) as a colapsible panel, use TripModeType instead of TripMotType

--- src/app/search-form/trip-mot-type/trip-mot-type.component.ts
+++ src/app/search-form/trip-mot-type/trip-mot-type.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SbbDialog } from '@sbb-esta/angular/dialog';
 
 import * as OJP from '../../shared/ojp-sdk/index'
@@ -7,11 +7,15 @@ import { DebugXmlPopoverComponent } from '../debug-xml-popover/debug-xml-popover
 
 import { MapService } from 'src/app/shared/services/map.service';
 import { UserTripService } from 'src/app/shared/services/user-trip.service';
-import { SbbSelectChange } from '@sbb-esta/angular/select';
 
 interface TripMotTypeDataModel {
   sectionRequestData: OJP.RequestData | null,
   isNotLastSegment: boolean
+}
+
+interface TripTransportModeData {
+  modeType: OJP.TripModeType,
+  transportModes: OJP.IndividualTransportMode[],
 }
 
 @Component({
@@ -20,25 +24,92 @@ interface TripMotTypeDataModel {
   styleUrls: ['./trip-mot-type.component.scss']
 })
 export class TripMotTypeComponent implements OnInit {
-  @Input() tripMotTypeIdx: number
+  @Input() tripModeTypeIdx: number
+  @ViewChild('settingsContainer') settingsContainer!: ElementRef; 
 
-  // Needed by the template
-  public motTypes: OJP.TripMotType[]
-  public tripMotType: OJP.TripMotType
+  public tripTransportModeData: TripTransportModeData[]
+
+  public tripModeType: OJP.TripModeType
+  public tripTransportModes: OJP.IndividualTransportMode[]
+  public tripTransportMode: OJP.IndividualTransportMode
 
   public tripMotTypeDataModel: TripMotTypeDataModel
 
-  constructor(private debugXmlPopover: SbbDialog, public userTripService: UserTripService, private mapService: MapService) {
-    this.motTypes = OJP.TripMotTypes
+  public settingsCollapseID: string
+  public endpointMinDurationS: string
+  public endpointMaxDurationS: string
+  public endpointMinDistanceS: string
+  public endpointMaxDistanceS: string
 
-    this.tripMotTypeIdx = 0
-    this.tripMotType = this.userTripService.tripMotTypes[0]
+  constructor(private debugXmlPopover: SbbDialog, public userTripService: UserTripService, private mapService: MapService) {
+    this.tripTransportModeData = [
+      {
+        modeType: 'monomodal',
+        transportModes: [
+          'public_transport',
+          'walking',
+          'cycle',
+          'car_self_driving',
+          'bicycle_rental',
+          'escooter_rental',
+          'car_sharing'
+        ]
+      },
+      {
+        modeType: 'mode_at_start',
+        transportModes: [
+          'walking',
+          'cycle',
+          'car_self_driving',
+          'bicycle_rental',
+          'escooter_rental',
+          'car_sharing'
+        ]
+      },
+      {
+        modeType: 'mode_at_end',
+        transportModes: [
+          'walking',
+          'bicycle_rental',
+          'escooter_rental',
+          'car_sharing'
+        ]
+      },
+      {
+        modeType: 'mode_at_start_end',
+        transportModes: [
+          'walking',
+          'bicycle_rental',
+          'escooter_rental',
+          'car_sharing'
+        ]
+      }
+    ]
+
+    this.tripModeType = this.tripTransportModeData[0].modeType
+    this.tripTransportModes = JSON.parse(JSON.stringify(this.tripTransportModeData[0].transportModes))
+    this.tripTransportMode = this.tripTransportModes[0]
+
+    this.tripModeTypeIdx = 0
 
     this.tripMotTypeDataModel = <TripMotTypeDataModel>{}
+
+    this.settingsCollapseID = 'mode_custom_mode_settings_NOT_READY_YET';
+    this.endpointMinDurationS = '2';
+    this.endpointMaxDurationS = '30';
+    this.endpointMinDistanceS = '100';
+    this.endpointMaxDistanceS = '20000';
   }
 
   ngOnInit() {
-    this.tripMotType = this.userTripService.tripMotTypes[this.tripMotTypeIdx]
+    this.tripModeType = this.userTripService.tripModeTypes[this.tripModeTypeIdx];
+
+    const tripTransportModeData = this.tripTransportModeData.find(tripTransportMode => {
+      return tripTransportMode.modeType === this.tripModeType;
+    }) ?? this.tripTransportModeData[0];
+
+    this.tripTransportModes = JSON.parse(JSON.stringify(tripTransportModeData.transportModes));
+    this.tripTransportMode = this.userTripService.tripTransportModes[this.tripModeTypeIdx];
 
     this.tripMotTypeDataModel.isNotLastSegment = !this.isLastSegment();
     this.tripMotTypeDataModel.sectionRequestData = null
@@ -46,37 +117,61 @@ export class TripMotTypeComponent implements OnInit {
     this.userTripService.activeTripSelected.subscribe(trip => {
       this.updateRequestDataModel()
     })
+
+    this.userTripService.defaultsInited.subscribe(nothing => {
+      this.userTripService.updateTripLocationCustomMode(this.tripModeTypeIdx);
+    })
+
+    this.settingsCollapseID = 'mode_custom_mode_settings_' + this.tripModeTypeIdx;
   }
 
   private isLastSegment(): boolean {
-    return this.tripMotTypeIdx === (this.userTripService.tripMotTypes.length - 1);
+    return this.tripModeTypeIdx === (this.userTripService.tripModeTypes.length - 1);
   }
 
   public handleTapOnMapButton() {
-    const location = this.userTripService.viaLocations[this.tripMotTypeIdx] ?? null
-    this.mapService.tryToCenterAndZoomToLocation(location)
+    const viaTripLocation = this.userTripService.viaTripLocations[this.tripModeTypeIdx] ?? null
+    this.mapService.tryToCenterAndZoomToLocation(viaTripLocation.location)
   }
 
   public computeViaName(): string {
-    const nextViaLocation = this.userTripService.viaLocations[this.tripMotTypeIdx] ??  null
-    if (nextViaLocation === null) {
+    const nextViaTripLocation = this.userTripService.viaTripLocations[this.tripModeTypeIdx] ??  null
+    if (nextViaTripLocation === null) {
       return ''
     }
 
-    const defaultName = nextViaLocation.geoPosition?.asLatLngString() ?? ''
+    const defaultName = nextViaTripLocation.location.geoPosition?.asLatLngString() ?? ''
 
     return defaultName
   }
 
   public removeVia() {
-    this.userTripService.removeViaAtIndex(this.tripMotTypeIdx)
+    this.userTripService.removeViaAtIndex(this.tripModeTypeIdx)
   }
 
-  public onOptionChange(event: SbbSelectChange) {
-    const motType = event.value as OJP.TripMotType
+  public onTripModeChange() {
+    const tripTransportModeData = this.tripTransportModeData.find(tripTransportMode => {
+      return tripTransportMode.modeType === this.tripModeType;
+    }) ?? null;
 
-    this.tripMotType = motType
-    this.userTripService.updateTripMotType(motType, this.tripMotTypeIdx)
+    if (tripTransportModeData === null) {
+      return;
+    }
+
+    this.tripTransportModes = JSON.parse(JSON.stringify(tripTransportModeData.transportModes));
+    this.tripTransportMode = this.tripTransportModes[0];
+
+    this.userTripService.updateTripMode(this.tripModeType, this.tripTransportMode, this.tripModeTypeIdx);
+    this.userTripService.updateTripLocationCustomMode(this.tripModeTypeIdx);
+  }
+
+  public onTransportModeChange() {
+    this.userTripService.updateTripMode(this.tripModeType, this.tripTransportMode, this.tripModeTypeIdx);
+    this.userTripService.updateTripLocationCustomMode(this.tripModeTypeIdx);
+
+    if (this.tripTransportMode === 'public_transport') {
+      this.settingsContainer.nativeElement.classList.remove('show');
+    }
   }
 
   private updateRequestDataModel() {
@@ -88,7 +183,7 @@ export class TripMotTypeComponent implements OnInit {
       return
     }
 
-    const journeySection = lastJourneyResponse.sections[this.tripMotTypeIdx] ?? null
+    const journeySection = lastJourneyResponse.sections[this.tripModeTypeIdx] ?? null
     if (journeySection === null) {
       this.tripMotTypeDataModel.sectionRequestData = null
       return
@@ -105,5 +200,43 @@ export class TripMotTypeComponent implements OnInit {
       const popover = dialogRef.componentInstance as DebugXmlPopoverComponent
       popover.updateRequestData(this.tripMotTypeDataModel.sectionRequestData)
     });
+  }
+
+  public computeTripModeTypeText(tripModeType: OJP.TripModeType): string {
+    const MapTripMotType: Record<OJP.TripModeType, string> = {
+      monomodal: 'Monomodal',
+      mode_at_start: 'Mode at Start',
+      mode_at_end: 'Mode at End',
+      mode_at_start_end: 'Mode at Start & End'
+    }
+
+    const text = MapTripMotType[tripModeType] ?? 'n/a';
+    return text;
+  }
+
+  public computeTripTransportModeText(transportMode: OJP.IndividualTransportMode): string {
+    const MapIndividualTransportMode: Record<OJP.IndividualTransportMode, string> = {
+      public_transport: 'Public Transport',
+      walking: 'Walking',
+      cycle: 'Own Bicycle',
+      escooter_rental: 'eScooter Sharing',
+      car_sharing: 'Car Sharing',
+      car_self_driving: 'Own Car',
+      bicycle_rental: 'Bicycle Sharing',
+      charging_station: 'Charging Stations'
+    }
+
+    const text = MapIndividualTransportMode[transportMode] ?? 'n/a';
+    
+    return text;
+  }
+
+  public onRestrictionsChange() {
+    const minDuration = parseInt(this.endpointMinDurationS, 10);
+    const maxDuration = parseInt(this.endpointMaxDurationS, 10);
+    const minDistance = parseInt(this.endpointMinDistanceS, 10);
+    const maxDistance = parseInt(this.endpointMaxDistanceS, 10);
+
+    this.userTripService.updateTripLocationRestrictions(minDuration, maxDuration, minDistance, maxDistance, this.tripModeTypeIdx);
   }
 }
