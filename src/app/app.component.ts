acf008add40a701c3fc3929530489bffea93b9ec
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-09-10 23:49:48 +0200

    Adds app tabs, first tab is for journey search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface AppTab {
+  link: string
+  label: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  public appTabs: AppTab[];
+
+  constructor() {
+    this.appTabs = [
+      {
+        label: 'Journey Search',
+        link: 'search'
+      },
+    ]
+  }
+
   ngOnInit() {
 
   }
