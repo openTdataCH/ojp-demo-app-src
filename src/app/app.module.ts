d2b0a992120e21613f42fc3b11aa699387bcf27f
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-09-10 23:45:13 +0200

    Hello JourneySearch component, placeholder for journeys search tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { InputXmlPopoverComponent } from './search-form/input-xml-popover/input-
 import { JourneyPointInputComponent } from './search-form/journey-point-input/journey-point-input.component';
 import { JourneyResultsComponent } from './journey/journey-results/journey-results.component';
 import { JourneyResultRowComponent } from './journey/journey-result-row/journey-result-row.component'
+import { JourneySearchComponent } from './journey/journey-search/journey-search.component';
 import { MapComponent } from './map/map.component';
 import { ResultTripLegComponent } from './journey/journey-result-row/result-trip-leg/result-trip-leg.component';
 import { SearchFormComponent } from './search-form/search-form.component';
@@ -29,6 +30,7 @@ import { TripModeTypeComponent } from './search-form/trip-mode-type/trip-mode-ty
     JourneyResultsComponent,
     JourneyResultRowComponent,
     JourneyPointInputComponent,
+    JourneySearchComponent,
     MapComponent,
     ResultTripLegComponent,
     SearchFormComponent,
---
+++ src/app/journey/journey-search/journey-search.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'journey-search',
+  templateUrl: './journey-search.component.html',
+})
+export class JourneySearchComponent implements OnInit {
+    ngOnInit(): void {
+        
+    }
+}
