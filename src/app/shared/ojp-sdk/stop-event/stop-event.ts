16ca4909a31ee80e05e08aae5103dd4630629a52
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-09-11 00:14:34 +0200

    Adds StationBoardModel to be used by the renderer component

--- src/app/shared/ojp-sdk/stop-event/stop-event.ts
+++ src/app/shared/ojp-sdk/stop-event/stop-event.ts
@@ -2,6 +2,30 @@ import { XPathOJP } from '../helpers/xpath-ojp'
 import { StopPoint } from '../trip/leg/timed-leg/stop-point'
 import { JourneyService } from '../journey/journey-service'
 import { Location } from '../location/location';
+import { StopPointTime } from '../trip';
+import { DateHelpers } from '../helpers/date-helpers';
+
+interface StationBoardTime {
+    stopTime: string
+    stopTimeActual: string | null
+    stopDelayText: string | null
+}
+
+export interface StationBoardModel {
+    stopEvent: StopEvent
+
+    serviceLineNumber: string
+    servicePtMode: string
+    tripNumber: string | null
+    tripHeading: string
+    tripOperator: string
+    
+    arrivalTime: StationBoardTime | null
+    departureTime: StationBoardTime | null
+    
+    stopPlatform: string | null
+    stopPlatformActual: string | null
+}
 
 export class StopEvent {
     public journeyService: JourneyService;
@@ -71,5 +95,85 @@ export class StopEvent {
                 stopPoint.location = mapContextLocations[stopPointRef];
             }
         });
+    }
+
+    public asStationBoard(): StationBoardModel {
+        const serviceLineNumber = this.computeServiceLineNumber()
+        const servicePtMode = this.journeyService.ptMode.shortName ?? 'N/A'
+
+        const tripNumber = this.journeyService.journeyNumber
+        const tripHeading = this.journeyService.destinationStopPlace?.stopPlaceName ?? 'N/A'
+        const tripOperator = this.journeyService.agencyID
+
+        const arrivalTime = this.computeStopTimeData(this.stopPoint.arrivalData)
+        const departureTime = this.computeStopTimeData(this.stopPoint.departureData)
+
+        const stopPlatform = this.stopPoint.plannedPlatform
+        const stopPlatformActual = this.stopPoint.actualPlatform
+
+        const model = <StationBoardModel>{
+            stopEvent: this,
+            serviceLineNumber: serviceLineNumber,
+            servicePtMode: servicePtMode,
+            tripNumber, tripHeading, tripOperator,
+            arrivalTime, departureTime,
+            stopPlatform, stopPlatformActual,
+        }
+
+        return model;
+    }
+
+    private computeServiceLineNumber(): string {
+        const serviceShortName = this.journeyService.ptMode.shortName ?? 'N/A';
+        const serviceLineNumber = this.journeyService.serviceLineNumber;
+        if (serviceLineNumber) {
+            return serviceLineNumber
+        } else {
+            return serviceShortName;
+        }
+    }
+
+    private computeStopTimeData(stopPointTime: StopPointTime | null): StationBoardTime | null {
+        if (stopPointTime === null) {
+            return null
+        }
+    
+        const stopTimeData = <StationBoardTime>{
+            stopTime: this.computeStopTime(stopPointTime.timetableTime ?? null),
+            stopTimeActual: this.computeStopTime(stopPointTime.estimatedTime ?? null),
+            stopDelayText: this.computeDelayTime(stopPointTime),
+        }
+    
+        return stopTimeData;
+    }
+
+    private computeStopTime(stopTime: Date | null): string | null {
+        if (stopTime === null) {
+          return null
+        }
+    
+        const stopTimeText = DateHelpers.formatTimeHHMM(stopTime);
+        
+        return stopTimeText;
+    }
+
+    private computeDelayTime(stopPointTime: StopPointTime): string | null {
+        const delayMinutes = stopPointTime.delayMinutes ?? null;
+        if (delayMinutes === null) {
+            return null;
+        }
+    
+        if (delayMinutes === 0) {
+            return 'ON TIME';
+        }
+    
+        const delayTextParts: string[] = []
+        delayTextParts.push(' ')
+        delayTextParts.push(delayMinutes > 0 ? '+' : '')
+        delayTextParts.push('' + delayMinutes)
+        delayTextParts.push("'");
+    
+        const delayText = delayTextParts.join('');
+        return delayText;
     }
 }
