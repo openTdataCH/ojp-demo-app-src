73297db6acfdf9f6b05949525d9e45e1c64cc227
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-09-12 23:53:53 +0200

    Store also category tags for POIs

--- src/app/shared/ojp-sdk/location/poi.ts
+++ src/app/shared/ojp-sdk/location/poi.ts
@@ -3,18 +3,29 @@ import { XPathOJP } from "../helpers/xpath-ojp"
 export class PointOfInterest {
   public code: string
   public name: string
+  public categoryTags: string[]
 
-  constructor(code: string, name: string) {
+  constructor(code: string, name: string, categoryTags: string[]) {
     this.code = code
     this.name = name
+    this.categoryTags = categoryTags
   }
 
   public static initFromContextNode(contextNode: Node): PointOfInterest | null {
     const code = XPathOJP.queryText('ojp:PointOfInterest/ojp:PointOfInterestCode', contextNode)
     const name = XPathOJP.queryText('ojp:PointOfInterest/ojp:PointOfInterestName/ojp:Text', contextNode)
+    
+    const categoryTags: string[] = []
+    const categoryNodes = XPathOJP.queryNodes('ojp:PointOfInterest/ojp:PointOfInterestCategory', contextNode);
+    categoryNodes.forEach(categoryNode => {
+      const categoryTag = XPathOJP.queryText('ojp:OsmTag/ojp:Value', categoryNode);
+      if (categoryTag) {
+        categoryTags.push(categoryTag);
+      }
+    })
 
     if (code && name) {
-      const poi = new PointOfInterest(code, name)
+      const poi = new PointOfInterest(code, name, categoryTags)
       return poi
     }
 
