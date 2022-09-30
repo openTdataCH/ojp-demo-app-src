89b5e70b3356c20590a5d3beddcd492a2d438ecb
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2022-09-30 10:42:57 +0200

    Adds category and subCategory via a convention of the PointOfInterestCategory order

--- src/app/shared/ojp-sdk/location/poi.ts
+++ src/app/shared/ojp-sdk/location/poi.ts
@@ -1,19 +1,28 @@
 import { XPathOJP } from "../helpers/xpath-ojp"
+import { GeoRestrictionPoiOSMTag } from "../types/geo-restriction.type"
 
 export class PointOfInterest {
   public code: string
   public name: string
+  public category: GeoRestrictionPoiOSMTag
+  public subCategory: string
   public categoryTags: string[]
 
-  constructor(code: string, name: string, categoryTags: string[]) {
+  constructor(code: string, name: string, category: GeoRestrictionPoiOSMTag, subCategory: string, categoryTags: string[]) {
     this.code = code
     this.name = name
+    this.category = category
+    this.subCategory = subCategory
     this.categoryTags = categoryTags
   }
 
   public static initFromContextNode(contextNode: Node): PointOfInterest | null {
     const code = XPathOJP.queryText('ojp:PointOfInterest/ojp:PointOfInterestCode', contextNode)
     const name = XPathOJP.queryText('ojp:PointOfInterest/ojp:PointOfInterestName/ojp:Text', contextNode)
+
+    if (!(code && name)) {
+      return null;
+    }
     
     const categoryTags: string[] = []
     const categoryNodes = XPathOJP.queryNodes('ojp:PointOfInterest/ojp:PointOfInterestCategory', contextNode);
@@ -24,11 +33,17 @@ export class PointOfInterest {
       }
     })
 
-    if (code && name) {
-      const poi = new PointOfInterest(code, name, categoryTags)
-      return poi
+    // HACK - the subCategory is the first item
+    // - we expect exactly 2 <ojp:PointOfInterestCategory> nodes
+    if (categoryTags.length != 2) {
+      debugger;
+      return null;
     }
 
-    return null
+    const category = categoryTags[1] as GeoRestrictionPoiOSMTag;
+    const subCategory = categoryTags[0];
+
+    const poi = new PointOfInterest(code, name, category, subCategory, categoryTags);
+    return poi
   }
 }
