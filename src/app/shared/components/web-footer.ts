c9c77407e6ccd83434bea99e8b7734ce73ac4738
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-06-03 15:36:51 +0200

    Updates Last Update

--- src/app/shared/components/web-footer.ts
+++ src/app/shared/components/web-footer.ts
@@ -17,7 +17,7 @@ export class WebFooterComponent implements OnInit {
   constructor() {
     this.model = {
       sdkVersion: OJP.SDK_VERSION,
-      lastUpdate: '17.May 2024',
+      lastUpdate: '03.June 2024',
     }
   }
 
