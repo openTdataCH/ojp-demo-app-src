0bc899830d58c5cb7e263400ac4a8cd25bd0c2ca
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-07-17 13:31:33 +0200

    Adds TripInfoService

---
+++ src/app/trip-info/trip-info.service.ts
@@ -0,0 +1,10 @@
+import { EventEmitter, Injectable } from "@angular/core";
+
+import * as OJP from 'ojp-sdk'
+import { LegStopPointData } from "../shared/components/service-stops.component";
+
+@Injectable( {providedIn: 'root'} )
+export class TripInfoService {
+    public tripInfoResultUpdated = new EventEmitter<OJP.TripInfoResult | null>()
+    public locationSelected = new EventEmitter<LegStopPointData>()
+}
