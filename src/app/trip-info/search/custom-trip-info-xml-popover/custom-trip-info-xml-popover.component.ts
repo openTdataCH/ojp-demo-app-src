1b7858c55152f90bc9aefcc472f09c8e5462c8d9
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-07-17 13:33:33 +0200

    Adds TripInfoSearchComponent

---
+++ src/app/trip-info/search/custom-trip-info-xml-popover/custom-trip-info-xml-popover.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { UserTripService } from 'src/app/shared/services/user-trip.service';
+
+import * as OJP from 'ojp-sdk'
+
+@Component({
+  selector: 'custom-trip-info-popover',
+  templateUrl: './custom-trip-info-xml-popover.component.html',
+})
+export class CustomTripInfoXMLPopoverComponent {
+  public customRequestXMLs: string
+  public customResponseXMLs: string
+
+  public isRunningRequest: boolean
+
+  @Output() customRequestSaved = new EventEmitter<OJP.TripInfoRequest>()
+  @Output() customResponseSaved = new EventEmitter<string>()
+
+  constructor(private userTripService: UserTripService) {
+    this.customRequestXMLs = '... wait'
+    this.customResponseXMLs = 'Paste custom OJP TripInfoRequest Response XML here...'
+
+    this.isRunningRequest = false
+  }
+
+  public parseCustomRequestXML() {
+    this.isRunningRequest = true
+
+    const stageConfig = this.userTripService.getStageConfig();
+    const request = OJP.TripInfoRequest.initWithRequestMock(this.customRequestXMLs, stageConfig);
+    request.fetchResponse().then(response => {
+      this.isRunningRequest = false;
+      const responseXML = request.requestInfo.responseXML;
+      if (responseXML === null) {
+        console.log('ERROR - no response from StopEventRequest');
+        return;
+      }
+
+      this.customRequestSaved.emit(request);
+    });
+  }
+
+  public parseCustomResponseXML() {
+    this.customResponseSaved.emit(this.customResponseXMLs)
+  }
+}
