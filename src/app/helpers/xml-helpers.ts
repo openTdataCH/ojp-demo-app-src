96e6fb4ffb7befe77aa012cc9bfcfae01368b047
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-10-02 18:19:46 +0200

    re-format XSLT helper

--- src/app/helpers/xml-helpers.ts
+++ src/app/helpers/xml-helpers.ts
@@ -3,33 +3,31 @@
 export class XML_Helpers {
   // from https://stackoverflow.com/a/47317538
   public static prettyPrintXML(sourceXml: string): string {
-    var xmlDoc = new DOMParser().parseFromString(sourceXml, 'application/xml');
-    var xsltDoc = new DOMParser().parseFromString(
-      [
-        '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
-        '  <xsl:strip-space elements="*"/>',
-        '  <xsl:template match="para[content-style][not(text())]">', // change to just text() to strip space in text nodes
-        '    <xsl:value-of select="normalize-space(.)"/>',
-        '  </xsl:template>',
-        '  <xsl:template match="node()|@*">',
-        '    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
-        '  </xsl:template>',
-        '  <xsl:output indent="yes"/>',
-        '</xsl:stylesheet>',
-      ].join('\n'),
-      'application/xml'
-    );
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(sourceXml, 'application/xml');
+    
+    const xsltString = `
+      <xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+        <xsl:strip-space elements="*"/>
+        <xsl:output indent="yes"/>
+        
+        <!-- change to just text() to strip space in text nodes -->
+        <xsl:template match="para[content-style][not(text())]">
+          <xsl:value-of select="normalize-space(.)"/>
+        </xsl:template>
+        <xsl:template match="node()|@*">
+          <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>
+        </xsl:template>
+      </xsl:stylesheet>
+    `;
+    const xsltDoc = parser.parseFromString(xsltString, 'application/xml');
+    
+    var xsltProcessor = new XSLTProcessor();
+    xsltProcessor.importStylesheet(xsltDoc);
+    const transformedDoc = xsltProcessor.transformToDocument(xmlDoc);
+    const serializer = new XMLSerializer();
+    const resultXml = serializer.serializeToString(transformedDoc);
 
-    try {
-      var xsltProcessor = new XSLTProcessor();
-      // TODO - check what's wrong in Firefox, why this fails
-      xsltProcessor.importStylesheet(xsltDoc);
-      var resultDoc = xsltProcessor.transformToDocument(xmlDoc);
-      var resultXml = new XMLSerializer().serializeToString(resultDoc);
-      return resultXml;
-    } catch (error) {
-      console.error('XML_Helpers.prettyPrintXML - fails, see TODO');
-      return sourceXml;
-    }
+    return resultXml;
   }
 }
