a2a2eac9913d37f65b2eea79fc05001102831e84
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-10-29 23:58:37 +0100

    Check the environment based on the current hostname

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -1,7 +1,18 @@
 import * as OJP from 'ojp-sdk'
 
 type DEBUG_LEVEL_Type = 'DEBUG' | 'PROD'
-export const DEBUG_LEVEL: DEBUG_LEVEL_Type = 'PROD'
+export const DEBUG_LEVEL: DEBUG_LEVEL_Type = (() => {
+  const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+  if (!isBrowser) {
+      return 'PROD';
+  }
+
+  if (window.location.hostname.includes('github.io')) {
+      return 'PROD';
+  }
+
+  return 'DEBUG';
+})();
 
 export type APP_STAGE = OJP.Default_APP_Stage
 
