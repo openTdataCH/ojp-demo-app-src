ba18a888af029ea539b59b9fc1641af3946819ed
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-12-05 22:49:52 +0100

    Adds map-colors from OJP SDK

---
+++ src/app/config/map-colors.ts
@@ -0,0 +1,28 @@
+import * as OJP from 'ojp-sdk'
+
+import { TripLegLineType } from '../shared/types/map-geometry-types'
+
+export const MapLegTypeColor: Record<OJP.LegType, string> = {
+  ContinousLeg: '#009933',
+  TransferLeg: '#0000ff',
+  TimedLeg: '#D04D44',
+}
+export const MapLegTypes: OJP.LegType[] = ['ContinousLeg', 'TimedLeg', 'TransferLeg']
+
+export const MapLegLineTypeColor: Record<TripLegLineType, string> = {
+  Unknown: '#ffafcc',
+  LongDistanceRail: '#D04D44',
+  SBahn: '#0d6efd',
+  Bus: '#0d6efd',
+  PostAuto: '#EBBE24',
+  OnDemand: '#D77D00',
+  Aerial: '#8B4513',
+  Funicular: '#006400',
+  Walk: '#009933',
+  'Self-Drive Car': '#ff004f',
+  'Shared Mobility': '#32CD32',
+  Guidance: '#6f0000',
+  Transfer: '#088F8F',
+  'Water': '#005AB3',
+}
+export const MapTripLegLineTypes: TripLegLineType[] = ['LongDistanceRail', 'SBahn', 'Bus', 'PostAuto', 'OnDemand', 'Aerial', 'Walk', 'Guidance', 'Transfer', 'Self-Drive Car', 'Shared Mobility', 'Water']
