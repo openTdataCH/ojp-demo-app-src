14cce932e17476c8afb89b23238de6a1866dca97
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-06 10:54:32 +0100

    Order elements

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -6,10 +6,6 @@ export const APP_CONFIG: AppConfig = {
       url: 'https://api.opentransportdata.swiss/ojp2020',
       authToken: null,
     },
-    'PROD-LB': {
-      url: 'https://tools.odpch.ch/tmp/cors-proxy?url=https://ojp.lb.prod.ojp.odpch.ch/ojp/ojp',
-      authToken: null,
-    },
     'INT': {
       url: 'https://odpch-api.clients.liip.ch/ojp-passiv-int',
       authToken: null,
@@ -20,10 +16,6 @@ export const APP_CONFIG: AppConfig = {
     },
     'LA Beta': {
       url: 'https://api.opentransportdata.swiss/ojp-la-aktiv',
-      authToken: null,
-    },
-    'OJP-SI': {
-      url: 'https://dev.simo.si/OpenAPI/LinkingAlpsBetaPhase/OJP',
       authToken: null,
     },
 
@@ -37,6 +29,15 @@ export const APP_CONFIG: AppConfig = {
     },
     'V2-TEST': {
       url: 'https://odpch-api.clients.liip.ch/ojp20-test',
+      authToken: null,
+    },
+
+    'PROD-LB': {
+      url: 'https://tools.odpch.ch/tmp/cors-proxy?url=https://ojp.lb.prod.ojp.odpch.ch/ojp/ojp',
+      authToken: null,
+    },
+    'OJP-SI': {
+      url: 'https://dev.simo.si/OpenAPI/LinkingAlpsBetaPhase/OJP',
       authToken: null,
     },
     
