969c075741fa496731f38bd5143cab4d70f3abc9
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-19 17:25:23 +0200

    Set main class programatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import OJP_Legacy from './config/ojp-legacy';
+import { AppService } from './shared/services/app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  ngOnInit() {
+  constructor(private appService: AppService) {
 
+  }
+
+  ngOnInit() {
+    document.body.classList.add(this.appService.bgMainClassName);
   }
 }
