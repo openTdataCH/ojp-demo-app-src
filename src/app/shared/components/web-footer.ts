f96fac5fe50fcea172b7eb4702099820bf7f2c5b
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-19 17:58:02 +0200

    Keep urls in component

--- src/app/shared/components/web-footer.ts
+++ src/app/shared/components/web-footer.ts
@@ -6,7 +6,9 @@ import { DEBUG_LEVEL } from '../../config/constants';
 type PageModel = {
   sdkVersion: string,
   lastUpdate: string,
-  isTopograhicPlaceMapLayerEnabled: boolean
+  isTopograhicPlaceMapLayerEnabled: boolean,
+  changelogURL: string,
+  githubURL: string,
 }
 
 @Component({
@@ -19,10 +21,22 @@ export class WebFooterComponent implements OnInit {
   constructor() {
     const isTopograhicPlaceMapLayerEnabled = DEBUG_LEVEL === 'DEBUG';
 
+    let changelogURL = 'https://github.com/openTdataCH/ojp-demo-app-src/blob/feature/ojp-v1-beta/CHANGELOG.md';
+    if (OJP_Legacy.OJP_VERSION === '2.0') {
+      changelogURL = 'https://github.com/openTdataCH/ojp-demo-app-src/blob/feature/ojp-v2-beta/CHANGELOG.md';
+    }
+
+    let githubURL = 'https://github.com/openTdataCH/ojp-js/blob/feature/ojp-v1/CHANGELOG.md';
+    if (OJP_Legacy.OJP_VERSION === '2.0') {
+      githubURL = 'https://github.com/openTdataCH/ojp-js/blob/feature/ojp-v2/CHANGELOG.md';
+    }
+
     this.model = {
       sdkVersion: OJP_Legacy.SDK_VERSION,
       lastUpdate: '2.April 2025',
       isTopograhicPlaceMapLayerEnabled: isTopograhicPlaceMapLayerEnabled,
+      changelogURL: changelogURL,
+      githubURL: githubURL,
     }
     
     // Disable all-together (i.e. the feature is not ready yet)
