3a50327b1ef78d7322ff6aec20f302e7596b0a98
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-19 17:24:45 +0200

    Updates title / header text programatically

--- src/app/journey/journey-search/journey-search.component.ts
+++ src/app/journey/journey-search/journey-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../shared/services/app.service';
 
 @Component({
   selector: 'journey-search',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./journey-search.component.scss'],
 })
 export class JourneySearchComponent implements OnInit {
-    ngOnInit(): void {
-        
-    }
+  constructor(private appService: AppService) {}
+
+  ngOnInit(): void {
+    this.appService.setTitle('TripRequest');
+  }
 }
