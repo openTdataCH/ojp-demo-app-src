644c994d587a57029c310d8110bf4fcbbed83c4b
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-05-02 13:50:38 +0200

    Adds TripData and TripLegData types for holding additional info on trips / legs

---
+++ src/app/shared/types/trip.ts
@@ -0,0 +1,17 @@
+import OJP_Legacy from '../../config/ojp-legacy';
+
+export interface TripLegData {
+  leg: OJP_Legacy.TripLeg,
+  info: {
+    id: string,
+    comments: string | null,
+  }
+}
+
+export interface TripData {
+  trip: OJP_Legacy.Trip
+  legsData: TripLegData[],
+  info: {
+    comments: string | null,
+  }
+};
