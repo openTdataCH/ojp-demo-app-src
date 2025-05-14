17875429070e750d36b0c480fabf0beafc8db69f
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-05-14 10:22:48 +0200

    Adds FareResult to TripData

--- src/app/shared/types/trip.ts
+++ src/app/shared/types/trip.ts
@@ -1,4 +1,5 @@
 import OJP_Legacy from '../../config/ojp-legacy';
+import * as OJP_Types from 'ojp-shared-types';
 
 export interface TripLegData {
   leg: OJP_Legacy.TripLeg,
@@ -9,7 +10,8 @@ export interface TripLegData {
 }
 
 export interface TripData {
-  trip: OJP_Legacy.Trip
+  trip: OJP_Legacy.Trip,
+  fareResult: OJP_Types.FareResultSchema | null,
   legsData: TripLegData[],
   info: {
     comments: string | null,
