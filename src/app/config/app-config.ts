bdba92887a2306907356ffefd28520bcfa8f19d6
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-06-06 12:21:49 +0200

    No more need in config

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -1,7 +1,4 @@
 import { AppConfig } from '../shared/types/app-config';
-import OJP_Legacy from './ojp-legacy';
-
-export const OJP_VERSION: OJP_Legacy.OJP_VERSION_Type = '2.0';
 
 export const APP_CONFIG: AppConfig = {
   stages: {
