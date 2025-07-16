50b0a608875891d5b57e777647cde159f271063e
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-07-16 10:55:17 +0200

    Updates footer date

--- src/app/shared/components/web-footer.ts
+++ src/app/shared/components/web-footer.ts
@@ -38,7 +38,7 @@ export class WebFooterComponent implements OnInit {
 
     this.model = {
       sdkVersionText: 'ojp-sdk-legacy: ' + OJP_Legacy.SDK_VERSION + ' - ojp-sdk-next: ' + OJP_Next.SDK_VERSION + ' - OJP API ' + OJP_VERSION,
-      lastUpdate: '25.June 2025',
+      lastUpdate: '16.July 2025',
       isTopograhicPlaceMapLayerEnabled: isTopograhicPlaceMapLayerEnabled,
       changelogURL: changelogURL,
       githubURL: githubURL,
