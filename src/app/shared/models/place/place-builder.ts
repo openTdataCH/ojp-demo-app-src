04b79453e6b674ee68e440dde00b4370ba988942
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-06 14:03:21 +0100

    Adds PlaceBuilder

---
+++ src/app/shared/models/place/place-builder.ts
@@ -0,0 +1,52 @@
+import * as OJP_SharedTypes from 'ojp-shared-types';
+
+import { Address } from './address';
+import { StopPlace } from './stop-place';
+import { Poi } from './poi';
+
+export type AnyPlace = Address | Poi | StopPlace;
+
+export class PlaceBuilder {
+  public static initWithPlaceResultSchema(placeResultSchema: OJP_SharedTypes.PlaceResultSchema): AnyPlace | null {
+    if (placeResultSchema.place.stopPlace || placeResultSchema.place.stopPoint) {
+      let place: StopPlace | null = null;
+      try {
+        place = StopPlace.initWithPlaceResultSchema(placeResultSchema);
+      } catch (error) {
+        console.error('StopPlace.initWithPlaceResultSchema:', error);
+        console.log(placeResultSchema);
+      }
+      if (place) {
+        return place;
+      }
+    }
+
+    if (placeResultSchema.place.address) {
+      let place: Address | null = null;
+      try {
+        place = Address.initWithPlaceResultSchema(placeResultSchema);
+      } catch (error) {
+        console.error('Address.initWithPlaceResultSchema:', error);
+        console.log(placeResultSchema);
+      }
+      if (place) {
+        return place;
+      }
+    }
+
+    if (placeResultSchema.place.pointOfInterest) {
+      let place: Poi | null = null;
+      try {
+        place = Poi.initWithPlaceResultSchema(placeResultSchema);
+      } catch (error) {
+        console.error('Address.initWithPlaceResultSchema:', error);
+        console.log(placeResultSchema);
+      }
+      if (place) {
+        return place;
+      }
+    }
+
+    return null;
+  }
+}
