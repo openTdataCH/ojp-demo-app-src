f74e00fcbf76e5b585339f140458a9293ae0e254
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-13 20:02:19 +0100

    TIR - use Location OJPv1 schema for context locations

--- src/app/shared/models/trip-info-result.ts
+++ src/app/shared/models/trip-info-result.ts
@@ -45,11 +45,27 @@ export class TripInfoResult {
       return null;
     }
 
-    const mapPlaces: Record<string, OJP_Next.Place> = {};
-    const places = tripInfoDeliverySchema.tripInfoResponseContext?.places?.place ?? [];
-    places.forEach(placeSchema => {
-      const place = OJP_Next.Place.initWithXMLSchema(placeSchema);
+    const places: OJP_Next.Place[] = [];
+    if (ojpVersion === '2.0') {
+      const tripInfoDeliverySchemaOJPv2 = tripInfoDeliverySchema as OJP_Types.TripInfoDeliverySchema;
 
+      const placesSchema = tripInfoDeliverySchemaOJPv2.tripInfoResponseContext?.places?.place ?? [];
+      placesSchema.forEach(placeSchema => {
+        const place = OJP_Next.Place.initWithXMLSchema(placeSchema);
+        places.push(place);
+      });
+    } else {
+      const tripInfoDeliverySchemaOJPv1 = tripInfoDeliverySchema as OJP_Types.OJPv1_TripInfoDeliverySchema;
+
+      const placesSchema = tripInfoDeliverySchemaOJPv1.tripInfoResponseContext?.places?.location ?? [];
+      placesSchema.forEach(placeSchema => {
+        const place = OJP_Next.Place.initWithOJPv1XMLSchema(placeSchema);
+        places.push(place);
+      });
+    }
+
+    const mapPlaces: Record<string, OJP_Next.Place> = {};
+    places.forEach(place => {
       const stopPlaceRef = place.stopPlace?.stopPlaceRef ?? null;
       if (stopPlaceRef) {
         mapPlaces[stopPlaceRef] = place;
