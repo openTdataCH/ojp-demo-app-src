5ea7a5c524ca00a5123fa66d89faff036257436a
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-16 12:17:46 +0100

    Updates imports

--- src/app/shared/models/trip-info-result.ts
+++ src/app/shared/models/trip-info-result.ts
@@ -1,6 +1,8 @@
 import * as OJP_Types from 'ojp-shared-types';
 import * as OJP_Next from 'ojp-sdk-next';
 
+import OJP_Legacy from '../../config/ojp-legacy';
+
 import { OJP_VERSION } from '../../config/constants';
 
 import { StopEventType, StopPointCall, StopPointCallType } from '../types/_all';
