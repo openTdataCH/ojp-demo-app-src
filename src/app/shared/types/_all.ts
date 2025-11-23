de19811a7d59964140aeb2a13af1ca23f43576d1
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-23 22:30:25 +0100

    No more need for type, can be used on each caller based on indexes

--- src/app/shared/types/_all.ts
+++ src/app/shared/types/_all.ts
@@ -5,7 +5,6 @@ import { AnyPlace } from '../models/place/place-builder';
 
 export type StopEventType = 'arrival' | 'departure';
 export type VehicleAccessType = 'PLATFORM_ACCESS_WITHOUT_ASSISTANCE' | 'PLATFORM_ACCESS_WITH_ASSISTANCE' | 'PLATFORM_ACCESS_WITH_ASSISTANCE_WHEN_NOTIFIED' | 'PLATFORM_NOT_WHEELCHAIR_ACCESSIBLE' | 'ALTERNATIVE_TRANSPORT' | 'NO_DATA';
-export type StopPointCallType = 'From' | 'To' | 'Intermediate';
 
 interface MapEventPlatformData {
   timetable: string | null,
@@ -20,7 +19,6 @@ export interface MapEventTimeData {
 };
 
 export interface StopPointCall {
-  type: StopPointCallType,
   place: AnyPlace | null,
   stopPointRef: string,
   stopPointName: string,
