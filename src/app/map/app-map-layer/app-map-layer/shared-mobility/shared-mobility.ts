41be5ddeaf2dbdec0ed6d0f269e21cead6b34795
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-12-08 17:10:39 +0100

    Adds new logic for scooters

--- src/app/map/app-map-layer/app-map-layer/shared-mobility/shared-mobility.ts
+++ src/app/map/app-map-layer/app-map-layer/shared-mobility/shared-mobility.ts
@@ -1,5 +1,6 @@
 import OJP_Legacy from '../../../../config/ojp-legacy';
 
+import { OJP_VERSION } from '../../../../config/constants';
 import { AnyPlace } from '../../../../shared/models/place/place-builder';
 import { Poi, RestrictionPoiOSMTag } from '../../../../shared/models/place/poi';
 
@@ -63,6 +64,36 @@ export class SharedMobility {
 
     // see https://github.com/SFOE/sharedmobility/blob/main/providers.csv
     const providerData: [SharedMobilityProvider | null, VehicleType] = (() => {
+      if (OJP_VERSION === '1.0') {
+        const placeName = (poi.placeName ?? 'n/a').toLowerCase();
+        if (placeName.startsWith('bolt')) {
+          return ['Bolt', 'E-Scooter'];
+        }
+        if (placeName.startsWith('lime')) {
+          return ['Lime', 'E-Scooter'];
+        }
+        if (placeName.startsWith('voi')) {
+          return ['VOI', 'E-Scooter'];
+        }
+        if (placeName.startsWith('bird')) {
+          return ['Bird', 'E-Scooter'];
+        }
+      } else {
+        const operatorName = (place.properties['OPERATOR_NAME'] ?? 'n/a').toLowerCase();
+        if (operatorName.startsWith('bird')) {
+          return ['Bird', 'E-Scooter'];
+        }
+        if (operatorName.startsWith('bolt')) {
+          return ['Bolt', 'E-Scooter'];
+        }
+        if (operatorName.startsWith('lime')) {
+          return ['Lime', 'E-Scooter'];
+        }
+        if (operatorName.startsWith('voi')) {
+          return ['VOI', 'E-Scooter'];
+        }
+      }
+
       if (poiCategory === 'bicycle_rental') {
         if (poi.placeName === 'Publibike') {
           return ['PubliBike', 'Bike'];
