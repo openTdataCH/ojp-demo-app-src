d1ef3385a82e26cc5f273cf0cfa94dec3756fd9a
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-12-30 19:22:52 +0100

    Cleanup imports

--- src/app/shared/services/user-trip.service.ts
+++ src/app/shared/services/user-trip.service.ts
@@ -9,7 +9,7 @@ import * as OJP_Next from 'ojp-sdk-next';
 import OJP_Legacy from '../../config/ojp-legacy';
 
 import { APP_CONFIG } from '../../config/app-config';
-import { APP_STAGE, DEBUG_LEVEL, DEFAULT_APP_STAGE, REQUESTOR_REF, TRIP_REQUEST_DEFAULT_NUMBER_OF_RESULTS, OJP_VERSION } from '../../config/constants';
+import { APP_STAGE, DEFAULT_APP_STAGE, REQUESTOR_REF, TRIP_REQUEST_DEFAULT_NUMBER_OF_RESULTS, OJP_VERSION } from '../../config/constants';
 
 import { MapService } from './map.service';
 import { TripData, TripLegData } from '../types/trip';
@@ -18,7 +18,6 @@ import { PlaceLocation } from '../models/place/location';
 import { GeoPositionBBOX } from '../models/geo/geoposition-bbox';
 import { OJPHelpers } from '../../helpers/ojp-helpers';
 import { AnyLocationInformationRequestResponse } from '../types/_all';
-import { TripLegGeoController } from '../controllers/trip-geo-controller';
 
 type LocationUpdateSource = 'SearchForm' | 'MapDragend' | 'MapPopupClick';
 
