b8119b0f0f421d41f7e1c8b67e38ab86badec459
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-01-30 16:10:43 +0100

    Expose more types, responses

--- src/app/shared/types/_all.ts
+++ src/app/shared/types/_all.ts
@@ -51,7 +51,12 @@ export interface CreateIssueBody {
 export type AnyPlaceSchema = OJP_Types.PlaceSchema | OJP_Types.OJPv1_LocationSchema;
 export type AnyPlaceResultSchema = OJP_Types.PlaceResultSchema | OJP_Types.OJPv1_LocationResultSchema;
 export type AnyPointOfInterestSchema = OJP_Types.PointOfInterestSchema | OJP_Types.OJPv1_PointOfInterestSchema;
+export type AnyTripResultSchema = OJP_Types.TripResultSchema | OJP_Types.OJPv1_TripResultSchema;
+export type AnyResponseContextSchema = OJP_Types.ResponseContextSchema | OJP_Types.OJPv1_ResponseContextSchema;
+export type AnyPtSituationElement = OJP_Types.PtSituationElement | OJP_Types.OJPv1_PtSituationElement;
+
 export type AnyLocationInformationRequest = OJP_Next.LocationInformationRequest | OJP_Next.OJPv1_LocationInformationRequest;
 export type AnyLocationInformationRequestResponse = OJP_Next.LocationInformationRequestResponse | OJP_Next.OJPv1_LocationInformationRequestResponse;
 export type AnyStopEventRequestResponse = OJP_Next.StopEventRequestResponse | OJP_Next.OJPv1_StopEventRequestResponse;
 export type AnyTripInfoRequestResponse = OJP_Next.TripInfoRequestResponse | OJP_Next.OJPv1_TripInfoRequestResponse;
+export type AnyTripRequestResponse = OJP_Next.TripRequestResponse | OJP_Next.OJPv1_TripRequestResponse;
