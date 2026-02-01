573f6dcd02832a114e02f4c124336c4e15ff2a79
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-02-01 22:57:56 +0100

    Use TR from ojp-sdk-next

--- src/app/search-form/input-xml-popover/input-xml-popover.component.ts
+++ src/app/search-form/input-xml-popover/input-xml-popover.component.ts
@@ -1,11 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
-import * as OJP_Next from 'ojp-sdk-next';
-import OJP_Legacy from '../../config/ojp-legacy';
-
-import { REQUESTOR_REF, OJP_VERSION } from '../../config/constants';
-
 import { UserTripService } from '../../shared/services/user-trip.service';
+import { LanguageService } from '../../shared/services/language.service';
 @Component({
   selector: 'input-xml-popover',
   templateUrl: './input-xml-popover.component.html',
@@ -19,41 +15,33 @@ export class InputXmlPopoverComponent {
   @Output() tripCustomRequestSaved = new EventEmitter<string>()
   @Output() tripCustomResponseSaved = new EventEmitter<string>()
 
-  constructor(private userTripService: UserTripService) {
+  constructor(private userTripService: UserTripService, private languageService: LanguageService) {
     this.inputTripRequestXML = '... loading'
     this.inputTripRequestResponseXML = 'Paste custom OJP TripRequest Response XML here...'
 
     this.isRunningTripRequest = false;
   }
 
-  public parseCustomRequestXML() {
-    this.isRunningTripRequest = true;
-
-    const isOJPv2 = OJP_VERSION === '2.0';
-    const xmlConfig = isOJPv2 ? OJP_Next.DefaultXML_Config : OJP_Next.XML_BuilderConfigOJPv1;
-
+  public async parseCustomRequestXML() {
     this.userTripService.mapActiveTripSelected.emit(null);
-    
-    const stageConfig = this.userTripService.getStageConfig();
-    const request = OJP_Legacy.TripRequest.initWithRequestMock(stageConfig, this.inputTripRequestXML, xmlConfig, REQUESTOR_REF);
-    request.fetchResponse().then(response => {
-      this.isRunningTripRequest = false;
 
-      if (response.message === 'ERROR') {
-        console.error('ERROR fetching OJP response');
-        console.log(response);
-        return;
-      }
+    const sdk = this.userTripService.createOJP_SDK_Instance(this.languageService.language);
+    const request = sdk.requests.TripRequest.initWithResponseMock(this.inputTripRequestXML);
 
+    this.isRunningTripRequest = true;
+    const response = await request.fetchResponse(sdk);
+    this.isRunningTripRequest = false;
+
+    if (response.ok) {
       const responseXML = request.requestInfo.responseXML;
-      if (responseXML === null) {
-        console.error('ERROR parsing OJP response');
-        console.log(request);
-        return;
+      if (responseXML) {
+        this.tripCustomRequestSaved.emit(responseXML);
       }
-
-      this.tripCustomRequestSaved.emit(responseXML);
-    });
+    } else {
+      console.error('ERROR fetching OJP response');
+      console.log(response);
+      return;
+    }
   }
 
   public parseCustomResponseXML() {
