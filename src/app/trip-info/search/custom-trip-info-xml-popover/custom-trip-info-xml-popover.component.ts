b79b20a1603395e398aa4c645fa29038a90cac39
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-02-03 11:43:44 +0100

    Formatting

--- src/app/trip-info/search/custom-trip-info-xml-popover/custom-trip-info-xml-popover.component.ts
+++ src/app/trip-info/search/custom-trip-info-xml-popover/custom-trip-info-xml-popover.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
-
 import * as OJP from 'ojp-sdk';
 
 import { UserTripService } from 'src/app/shared/services/user-trip.service';
@@ -11,23 +10,23 @@ import { LanguageService } from '../../../shared/services/language.service';
   templateUrl: './custom-trip-info-xml-popover.component.html',
 })
 export class CustomTripInfoXMLPopoverComponent {
-  public customRequestXMLs: string
-  public customResponseXMLs: string
+  public customRequestXMLs: string;
+  public customResponseXMLs: string;
 
-  public isRunningRequest: boolean
+  public isRunningRequest: boolean;
 
-  @Output() customRequestSaved = new EventEmitter<OJP.RequestInfo>()
-  @Output() customResponseSaved = new EventEmitter<string>()
+  @Output() customRequestSaved = new EventEmitter<OJP.RequestInfo>();
+  @Output() customResponseSaved = new EventEmitter<string>();
 
   constructor(private userTripService: UserTripService, private languageService: LanguageService) {
-    this.customRequestXMLs = '... wait'
-    this.customResponseXMLs = 'Paste custom OJP TripInfoRequest Response XML here...'
+    this.customRequestXMLs = '... wait';
+    this.customResponseXMLs = 'Paste custom OJP TripInfoRequest Response XML here...';
 
-    this.isRunningRequest = false
+    this.isRunningRequest = false;
   }
 
   public async parseCustomRequestXML() {
-    this.isRunningRequest = true
+    this.isRunningRequest = true;
 
     const ojpSDK = this.userTripService.createOJP_SDK_Instance(this.languageService.language);
     const request = ojpSDK.requests.TripInfoRequest.initWithRequestMock(this.customRequestXMLs);
