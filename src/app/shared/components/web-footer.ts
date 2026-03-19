995c87426f40703bf0c0a8dbc180fdf2586cb4b0
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-03-19 16:39:22 +0100

    Updates footer day text

--- src/app/shared/components/web-footer.ts
+++ src/app/shared/components/web-footer.ts
@@ -33,7 +33,7 @@ export class WebFooterComponent implements OnInit {
     const isTopograhicPlaceMapLayerEnabled = DEBUG_LEVEL === 'DEBUG';
 
     this.model = {
-      lastUpdate: '17.Mar 2026',
+      lastUpdate: '19.Mar 2026',
       isTopograhicPlaceMapLayerEnabled: isTopograhicPlaceMapLayerEnabled,
       rasterLayers: MAP_RASTER_LAYERS,
       mapSDK_Versions: {
