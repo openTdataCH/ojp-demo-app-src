05b5e928b9887a459c83e7b179e93754a0608a66
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-06-02 09:33:43 +0200

    Directly use app service

--- src/app/shared/components/web-header.ts
+++ src/app/shared/components/web-header.ts
@@ -1,23 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService } from '../services/app.service';
 
-interface PageModel {
-  headerText: string,
-};
+import { AppService } from '../services/app.service';
 
 @Component({
   selector: 'web-header',
   templateUrl: './web-header.html',
 })
 export class WebHeaderComponent implements OnInit {
-  public model: PageModel;
+  constructor(public appService: AppService) {
 
-  constructor(private appService: AppService) {
-    this.model = {
-      headerText: this.appService.baseTitle,
-    };
   }
 
   ngOnInit(): void {
+  
   }
 }
