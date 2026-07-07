4510fdd50ef83dc3959822cce0775a6f30e44c21
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-07-07 08:35:10 +0200

    Adds helper for converting StopPlace to ZVV stopId

--- src/app/helpers/data-helpers.ts
+++ src/app/helpers/data-helpers.ts
@@ -1,3 +1,5 @@
+import { StopPlace } from "../shared/models/place/stop-place";
+
 export class DataHelpers {
   public static convertStopPointToStopPlace(stopPointRef: string): string {
     if (typeof(stopPointRef) === 'number') {
@@ -33,5 +35,24 @@ export class DataHelpers {
     console.log(countryMatches);
 
     return stopPointRef;
+  }
+
+  public static generateZVV_Id(stopPlace: StopPlace): string {
+    const stopPlaceRef = DataHelpers.convertStopPointToStopPlace(stopPlace.placeRef.ref);
+
+    // A=1@O=Gurten Kulm@X=7439751@Y=46919610@U=90@L=8507099@p=1773077090@
+    const idParts: string[] = [
+      'A=1',
+      'O=' + stopPlace.computeName(),
+      'X=' + stopPlace.geoPosition.longitude.toString().replace('.', ''),
+      'Y=' + stopPlace.geoPosition.latitude.toString().replace('.', ''),
+      'L=' + stopPlaceRef,
+      'p=1773077090',
+      ''
+    ];
+
+    const zvvId = idParts.join('@');
+
+    return zvvId;
   }
 }
